ad9b93a034efbecd8ed92ae2e7e8cc26514b90eb
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2021-03-24 18:35:32 +0530

    Sample included

---
+++ ChartSample/ChartSample/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ChartModel
+    {
+        public string XValue { get; set; }
+
+        public double YValue { get; set; }
+    
+    }
+    public class ViewModel
+    {
+        public ObservableCollection<ChartModel> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<ChartModel>();
+
+            Data.Add(new ChartModel() { XValue = "United States", YValue = 370 });
+            Data.Add(new ChartModel() { XValue = "India", YValue = 370 });
+            Data.Add(new ChartModel() { XValue = "Australia", YValue = 390 });
+            Data.Add(new ChartModel() { XValue = "Kuwait", YValue = 430 });
+            Data.Add(new ChartModel() { XValue = "China", YValue = 480 });
+         
+        }
+    }
+}
